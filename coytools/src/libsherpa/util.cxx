cd034a411dfcfdd0cf082b32e743f9b73a327b78
Jonathan S. Shapiro <shap@eros-os.com>
2008-07-11 17:21:25 -0400

    Split out the hash_string operation into a separate file.
    Previously, calling anything in util.hxx had the effect of
    inducing a requirement on openssl for the SHA1 encoder.

--- coytools/src/libsherpa/util.cxx
+++ coytools/src/libsherpa/util.cxx
@@ -57,341 +57,326 @@
 #include "sha1.hxx"
 
 namespace sherpa {
-std::string
-strdowncase(const std::string& s)
-{
-  std::string new_s = s;
+  std::string
+  strdowncase(const std::string& s)
+  {
+    std::string new_s = s;
 
-  for (size_t i = 0; i < new_s.size(); i++) {
-    if (isalpha(new_s[i]))
-      new_s.replace(i, 1, 1, tolower(new_s[i]));
+    for (size_t i = 0; i < new_s.size(); i++) {
+      if (isalpha(new_s[i]))
+	new_s.replace(i, 1, 1, tolower(new_s[i]));
+    }
+
+    return new_s;
   }
 
-  return new_s;
-}
+  /* Before this used sprintf. I grepped around, and found that this function
+     was only called _ONCE_, in Browse.c
 
-/* Before this used sprintf. I grepped around, and found that this function
-   was only called _ONCE_, in Browse.c
+     Initially, I changed Browse.c to use xunsigned64_str, and removed
+     xunsigned_str all together. Then I realized people (Shap+JV) might get mad
+     that this function wasn't around any more, blah blah blah, so I put it back.
 
-   Initially, I changed Browse.c to use xunsigned64_str, and removed
-   xunsigned_str all together. Then I realized people (Shap+JV) might get mad
-   that this function wasn't around any more, blah blah blah, so I put it back.
-
-   In theory this is less efficient than sprintf, but all in all it doesn't
-   really matter. It's not called (at all), and using xunsigned*_str implies
-   doing some form of I/O (always very expensive).
+     In theory this is less efficient than sprintf, but all in all it doesn't
+     really matter. It's not called (at all), and using xunsigned*_str implies
+     doing some form of I/O (always very expensive).
 
      JL (9/12/02)
-*/
-std::string unsigned_str(uint32_t n)
-{
- return unsigned64_str(n);
-}
+  */
+  std::string unsigned_str(uint32_t n)
+  {
+    return unsigned64_str(n);
+  }
 
-/* This helper routine is here because the logging logic should not
- * rely on the availability of mallocable memory. It should not be
- * called by anyone else! */
+  /* This helper routine is here because the logging logic should not
+   * rely on the availability of mallocable memory. It should not be
+   * called by anyone else! */
 #define BUFSZ 20 /* 19 for digits + 1 for null */
  
-static char *
-unsigned64_to_buffer(uint64_t ul, char *buf)
-{
-  char *p;
+  static char *
+  unsigned64_to_buffer(uint64_t ul, char *buf)
+  {
+    char *p;
 
-  buf[BUFSZ-1] = 0;
-  p = &buf[BUFSZ-1];
+    buf[BUFSZ-1] = 0;
+    p = &buf[BUFSZ-1];
 
-  if (ul == 0)
-    *(--p) = '0';
+    if (ul == 0)
+      *(--p) = '0';
 
-  while(ul) {
-    *(--p) = '0' + (ul % 10);
-    ul = ul / 10;
+    while(ul) {
+      *(--p) = '0' + (ul % 10);
+      ul = ul / 10;
+    }
+
+    return p;
   }
 
-  return p;
-}
+  /* bc says that 2^64-1==18446744073709551615, which looks to me like
+     19 characters. */
+  std::string
+  unsigned64_str(uint64_t ul)
+  {
+    char buf[BUFSZ];
+    char *p = unsigned64_to_buffer(ul, buf);
 
-/* bc says that 2^64-1==18446744073709551615, which looks to me like
-   19 characters. */
-std::string
-unsigned64_str(uint64_t ul)
-{
-  char buf[BUFSZ];
-  char *p = unsigned64_to_buffer(ul, buf);
-
-  return p;
-}
-
-uint64_t
-atollu(const char *s)
-{
-  uint64_t ull = 0;
-
-  for ( ; *s; s++) {
-    ull *= 10;
-    ull += (*s - '0');
+    return p;
   }
 
-  return ull;
-}
+  uint64_t
+  atollu(const char *s)
+  {
+    uint64_t ull = 0;
 
-uint64_t
-atollu(const std::string& s)
-{
-  uint64_t ull = 0;
+    for ( ; *s; s++) {
+      ull *= 10;
+      ull += (*s - '0');
+    }
 
-  for (size_t i = 0; i < s.size(); i++) {
-    ull *= 10;
-    ull += (s[i] - '0');
+    return ull;
   }
 
-  return ull;
-}
+  uint64_t
+  atollu(const std::string& s)
+  {
+    uint64_t ull = 0;
+
+    for (size_t i = 0; i < s.size(); i++) {
+      ull *= 10;
+      ull += (s[i] - '0');
+    }
+
+    return ull;
+  }
 
 #if 0
-FILE *xprintf_outfile = 0;
+  FILE *xprintf_outfile = 0;
 
-/* The only reason this exists is because I don't want to have to
- * bother being careful about printing null strings. Some
- * implementations of printf() behave gracelessly about that.
- */
+  /* The only reason this exists is because I don't want to have to
+   * bother being careful about printing null strings. Some
+   * implementations of printf() behave gracelessly about that.
+   */
 
 #define xputchar(c)  putc(c, xprintf_outfile)
 
-/* This is derived from the EROS kprintf code */
-size_t
-xprintf(const char *fmt, ...)
-{
-  unsigned long len;
-  unsigned long width = 0;
-  bool sign;
-  bool rightAdjust;
-  char fillchar;
-  char buf[20];
-  char *p, *pend;
-  size_t output_count = 0;
+  /* This is derived from the EROS kprintf code */
+  size_t
+  xprintf(const char *fmt, ...)
+  {
+    unsigned long len;
+    unsigned long width = 0;
+    bool sign;
+    bool rightAdjust;
+    char fillchar;
+    char buf[20];
+    char *p, *pend;
+    size_t output_count = 0;
   
-  va_list ap;
+    va_list ap;
 
-  if (xprintf_outfile == 0)
-    xprintf_outfile = stdout;
+    if (xprintf_outfile == 0)
+      xprintf_outfile = stdout;
 
-  va_start(ap, fmt);
+    va_start(ap, fmt);
     
-  for( ; *fmt; fmt++) {
-    if (*fmt != '%') {
-      xputchar(*fmt);
-      output_count ++;
-      continue;
-    }
-
-    /* largest thing we might convert fits in 20 digits (unsigned long
-     * long as decimal */
-    
-    pend = &buf[20];
-    p = pend;
-
-    fmt++;			/* now looking at specification */
-
-    /* check for left adjust.. */
-    rightAdjust = true;
-    if (*fmt == '-') {
-      rightAdjust = false;
-      fmt++;
-    }
-      
-    fillchar = ' ';
-    
-    /* we just saw a format character.  See if what follows
-     * is a width specifier:
-     */
-    width = 0;
-
-    if (*fmt == '0')
-      fillchar = '0';
-
-    while (*fmt && *fmt >= '0' && *fmt <= '9') {
-      width *= 10;
-      width += (*fmt - '0');
-      fmt++;
-    }
-    
-    assert (*fmt);		/* check bogus fmt */
-
-    sign = false;
-    
-    switch (*fmt) {
-    case '%':
-      {
+    for( ; *fmt; fmt++) {
+      if (*fmt != '%') {
 	xputchar(*fmt);
 	output_count ++;
-	break;
+	continue;
       }
-    case 's':
-      {
-	p = pend = va_arg(ap, char *);
+
+      /* largest thing we might convert fits in 20 digits (unsigned long
+       * long as decimal */
+    
+      pend = &buf[20];
+      p = pend;
+
+      fmt++;			/* now looking at specification */
+
+      /* check for left adjust.. */
+      rightAdjust = true;
+      if (*fmt == '-') {
+	rightAdjust = false;
+	fmt++;
+      }
       
-	if (pend == 0)
-	  p = pend = "<null>";
+      fillchar = ' ';
+    
+      /* we just saw a format character.  See if what follows
+       * is a width specifier:
+       */
+      width = 0;
 
-	while (*pend)
-	  pend++;
+      if (*fmt == '0')
+	fillchar = '0';
+
+      while (*fmt && *fmt >= '0' && *fmt <= '9') {
+	width *= 10;
+	width += (*fmt - '0');
+	fmt++;
+      }
+    
+      assert (*fmt);		/* check bogus fmt */
+
+      sign = false;
+    
+      switch (*fmt) {
+      case '%':
+	{
+	  xputchar(*fmt);
+	  output_count ++;
+	  break;
+	}
+      case 's':
+	{
+	  p = pend = va_arg(ap, char *);
+      
+	  if (pend == 0)
+	    p = pend = "<null>";
+
+	  while (*pend)
+	    pend++;
 	  
-	break;
-      }
-    case 'c':
-      {
-	long c;
-	c = va_arg(ap, long);
-	*(--p) = (char) c;
-	break;
-      }	    
-    case 'd':
-      {
-	long l;
-	unsigned long ul;
+	  break;
+	}
+      case 'c':
+	{
+	  long c;
+	  c = va_arg(ap, long);
+	  *(--p) = (char) c;
+	  break;
+	}	    
+      case 'd':
+	{
+	  long l;
+	  unsigned long ul;
 
-	l = va_arg(ap, long);
+	  l = va_arg(ap, long);
 	      
-	if (l == 0) {
-	  *(--p) = '0';
-	}
-	else {
-	  if (l < 0)
-	    sign = '-';
-
-	  ul = (l < 0) ? (unsigned) -l : (unsigned) l;
-
-	  if (l == LONG_MIN)
-	    ul = ((unsigned long) LONG_MAX) + 1ul;
-
-	  while(ul) {
-	    *(--p) = '0' + (ul % 10);
-	    ul = ul / 10;
+	  if (l == 0) {
+	    *(--p) = '0';
 	  }
-	}
-	break;
-      }
-    case 'u':
-      {
-	unsigned long ul;
+	  else {
+	    if (l < 0)
+	      sign = '-';
 
-	ul = va_arg(ap, unsigned long);
+	    ul = (l < 0) ? (unsigned) -l : (unsigned) l;
+
+	    if (l == LONG_MIN)
+	      ul = ((unsigned long) LONG_MAX) + 1ul;
+
+	    while(ul) {
+	      *(--p) = '0' + (ul % 10);
+	      ul = ul / 10;
+	    }
+	  }
+	  break;
+	}
+      case 'u':
+	{
+	  unsigned long ul;
+
+	  ul = va_arg(ap, unsigned long);
 	      
-	if (ul == 0) {
-	  *(--p) = '0';
-	}
-	else {
-	  while(ul) {
-	    *(--p) = '0' + (ul % 10);
-	    ul = ul / 10;
+	  if (ul == 0) {
+	    *(--p) = '0';
 	  }
+	  else {
+	    while(ul) {
+	      *(--p) = '0' + (ul % 10);
+	      ul = ul / 10;
+	    }
+	  }
+	  break;
 	}
-	break;
-      }
-    case 'x':
-      {
-	unsigned long ul;
-	static char *hex_digits = "0123456789abcdef";
+      case 'x':
+	{
+	  unsigned long ul;
+	  static char *hex_digits = "0123456789abcdef";
 
-	ul = va_arg(ap, unsigned long);
+	  ul = va_arg(ap, unsigned long);
 	      
-	if (ul == 0) {
-	  *(--p) = '0';
-	}
-	else {
-	  while(ul) {
-	    *(--p) = hex_digits[ul % 16];
-	    ul = ul / 16;
+	  if (ul == 0) {
+	    *(--p) = '0';
 	  }
+	  else {
+	    while(ul) {
+	      *(--p) = hex_digits[ul % 16];
+	      ul = ul / 16;
+	    }
+	  }
+	  break;
 	}
-	break;
+      default:
+	{
+	  assert(false);
+	}
       }
-    default:
-      {
-	assert(false);
-      }
-    }
   
-    len = pend - p;
-    if (sign)
-      len++;
+      len = pend - p;
+      if (sign)
+	len++;
 
-    /* do padding with initial spaces for right justification: */
-    if (width && rightAdjust && len < width) {
-      while (len < width) {
-	xputchar(fillchar);
-	output_count ++;
-	width--;
+      /* do padding with initial spaces for right justification: */
+      if (width && rightAdjust && len < width) {
+	while (len < width) {
+	  xputchar(fillchar);
+	  output_count ++;
+	  width--;
+	}
       }
-    }
 
-    if (sign)
-      xputchar(sign);
+      if (sign)
+	xputchar(sign);
 
-    /* output the text */
-    while (p != pend) {
-      xputchar(*p++);
-      output_count ++;
+      /* output the text */
+      while (p != pend) {
+	xputchar(*p++);
+	output_count ++;
+      }
+    
+      /* do padding with trailing spaces for left justification: */
+      if (width && rightAdjust == false && len < width) {
+	while (len < width) {
+	  xputchar(fillchar);
+	  output_count ++;
+	  width--;
+	}
+      }
     }
     
-    /* do padding with trailing spaces for left justification: */
-    if (width && rightAdjust == false && len < width) {
-      while (len < width) {
-	xputchar(fillchar);
-	output_count ++;
-	width--;
-      }
-    }
+    va_end(ap);
+
+    return output_count;
   }
-    
-  va_end(ap);
-
-  return output_count;
-}
 #endif
 
-/* This is _exactly_ like bsearch, except that if base == NULL, simply return
-   NULL. Normally this should work anyway, but it seems Solaris has 'issues'
-   with this -- thus, this wrapper function.
-*/
-void* 
-xbsearch(const void *key, const void *base, size_t nmemb,
-	 size_t size, int (*compar)(const void *, const void *))
-{
-  /* FIX: Should we check for base==NULL and nmemb > 0? */
-  if(base == NULL)
-    return NULL;
-  return bsearch(key, base, nmemb, size, compar);
-}
+  /* This is _exactly_ like bsearch, except that if base == NULL, simply return
+     NULL. Normally this should work anyway, but it seems Solaris has 'issues'
+     with this -- thus, this wrapper function.
+  */
+  void* 
+  xbsearch(const void *key, const void *base, size_t nmemb,
+	   size_t size, int (*compar)(const void *, const void *))
+  {
+    /* FIX: Should we check for base==NULL and nmemb > 0? */
+    if(base == NULL)
+      return NULL;
+    return bsearch(key, base, nmemb, size, compar);
+  }
 
-hash32_t
-hash_string(std::string s)
-{
-  OpenSHA sha(s);
-  hash32_t h = 0;
-  ByteString hd = sha.byteRepresentation();
-  
-  h |= ( ((hash32_t) hd[0]) << 24 );
-  h |= ( ((hash32_t) hd[1]) << 16 );
-  h |= ( ((hash32_t) hd[2]) << 8 );
-  h |= ( ((hash32_t) hd[3]) );
+  std::string
+  sgetenv(const char *var)
+  {
+    std::string s;
 
-  return h;
-}
+    const char *result = getenv(var);
 
-std::string
-sgetenv(const char *var)
-{
-  std::string s;
+    if (result)
+      s = result;
 
-  const char *result = getenv(var);
-
-  if (result)
-    s = result;
-
-  return s;
-}
+    return s;
+  }
 
 } /* namespace sherpa */
